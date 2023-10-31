d54f1af141997be65ebfc1b1fba15919fa120d05
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-10-31 16:05:16 +0100

    add some data

--- src/WeatherApp.Web/Infrastructure/Services/HttpWeatherForecastProvider.cs
+++ src/WeatherApp.Web/Infrastructure/Services/HttpWeatherForecastProvider.cs
@@ -2,10 +2,16 @@
 
 namespace WeatherApp.Web.Infrastructure.Services;
 
-public sealed class HttpWeatherForecastProvider : IWeatherForecastProvider
+.public sealed class HttpWeatherForecastProvider(HttpClient client) : IWeatherForecastProvider
 {
     public async Task<DayByDayWeatherForecast> GetWeatherForecastAsync(GetWeatherForecastQuery query, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var uri = new Uri(
+            $"https://api.openweathermap.org/data/3.0/onecall?lat={query.Coordinates.Latitude}&lon={query.Coordinates.Longitude}&appid={{API key}}");
+        using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+        var response = await client.SendAsync(request, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadFromJsonAsync<dynamic>(cancellationToken: cancellationToken);
     }
 }
---
+++ src/WeatherApp.Web/Infrastructure/Types/OpenWeatherResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace WeatherApp.Web.Infrastructure.Types;
+
+public sealed class OpenWeatherData
+{
+    public double Temp { get; set; }
+    public double FeelsLike { get; set; }
+}
+
+public sealed class OpenWeatherResponse
+{
+    public decimal Lat { get; set; }
+    public decimal Lon { get; set; }
+    public string Timezone { get; set; }
+    public int TimezoneOffset { get; set; }
+    public IReadOnlyCollection<OpenWeatherData> Data { get; set; }
+    
+}
