830fe6afbf45977509746840ba8b6ce16ab8898a
Dominik.Kotecki <dominikus1910@gmail.com>
2023-11-18 17:25:46 +0100

    init

--- src/WeatherApp.Web/Core/Services/IWeatherForecastProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WeatherApp.Web.Core.Services;
-
-public readonly record struct Coordinates(double Latitude, double Longitude);
-public sealed record GetWeatherForecastQuery(Coordinates Coordinates, DateOnly Date);
-
-public interface IWeatherForecastProvider
-{
-    Task<DayByDayWeatherForecast> GetWeatherForecastAsync(GetWeatherForecastQuery query, CancellationToken cancellationToken = default);
-}
